1090429db942e577965d6de0f208e8cb54387d45
Harish Balakrishnan <harish.balakrishnan.93@gmail.com>
2019-01-31 12:42:59 +0530

    Initialize app's data

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -5,8 +5,8 @@ import {setAuthedUser} from '../actions/authedUser'
 
 const AUTHED_ID = 'tylermcginnis'
 
-//handleInitialData thunk action creator - Returns function instead of action for thunk to invoke the dispatch functions given here 
-export function handleInitialData(){
+//handleInitialData thunk action creator - Returns function instead of action for thunk to invoke the dispatch functions given here
+export default function handleInitialData(){
   return (dispatch) => {
     getInitialData()
     .then(({users,tweets}) => {
