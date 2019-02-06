32bbed29f69f48dc9b33fd58982400b6f87eca0a
Harish Balakrishnan <harish.balakrishnan.93@gmail.com>
2019-02-06 19:48:07 +0530

    Added Like functionality

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,9 +1,35 @@
+import {saveLikeToggle} from '../utils/api'
+
 export const RECEIVE_TWEETS='RECEIVE_TWEETS'
+export const TOGGLE_TWEET='TOGGLE_TWEET'
 
 //receiveTweets action creator
 export function receiveTweets(tweets){
   return{
     type: RECEIVE_TWEETS,
     tweets
+  }
+}
+
+//toggleTweet action creator for liking tweets
+function toggleTweet({id,authedUser,hasLiked}){
+  return {
+    type: TOGGLE_TWEET,
+    id,
+    authedUser,
+    hasLiked
+  }
+}
+
+//handleToggleTweet thunk action creator to handle liking of the tweet - Optimistic Updates
+export function handleToggleTweet(info){
+  return (dispatch) => {
+    dispatch(toggleTweet(info))
+    return saveLikeToggle(info)
+    .catch((e) => {
+      console.warn('Error in handleToggleTweet: ',e)
+      dispatch(toggleTweet(info))
+      alert('There was an error liking the tweet. Please try again later.')
+    })
   }
 }
